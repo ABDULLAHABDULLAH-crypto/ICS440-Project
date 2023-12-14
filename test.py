eaaedc42d8478ec5388e919e5a452d9d7b5f8d29
Faris Alghamdi <f.k.a.m.ksa@gmail.com>
2023-12-14 13:57:26 +0300

    simplification

--- test.py
+++ test.py
@@ -1,27 +1,50 @@
+import secrets
 import time
-from Cryptodome.Cipher import AES
-import custom_AES, AES_128, AES_320, AES_384, AES_512
+from AES_version import AESVersion
+from custom_AES import CustomAES
 
-def measure_execution_time(function, *args, **kwargs):
+
+def generate_random_bytes(num_bytes: int) -> bytes:
+    return secrets.token_bytes(num_bytes)
+
+
+plaintext = generate_random_bytes(16 * 10)
+
+# Define keys for each AES version
+key_128 = generate_random_bytes(16)
+key_192 = generate_random_bytes(24)
+key_256 = generate_random_bytes(32)
+key_320 = generate_random_bytes(40)
+key_384 = generate_random_bytes(48)
+key_512 = generate_random_bytes(64)
+
+# Initialize AES objects for each version
+aes_versions = {
+    AESVersion.AES_128: (CustomAES(AESVersion.AES_128), key_128),
+    AESVersion.AES_192: (CustomAES(AESVersion.AES_192), key_192),
+    AESVersion.AES_256: (CustomAES(AESVersion.AES_256), key_256),
+    AESVersion.AES_320: (CustomAES(AESVersion.AES_320), key_320),
+    AESVersion.AES_384: (CustomAES(AESVersion.AES_384), key_384),
+    AESVersion.AES_512: (CustomAES(AESVersion.AES_512), key_512),
+}
+
+# Dictionary to store execution times
+execution_times = {}
+
+# Measure execution time for each AES version
+for version, (aes, key) in aes_versions.items():
     start_time = time.time()
-    function(*args, **kwargs)
+    encrypted = aes.encrypt(plaintext, key)
     end_time = time.time()
-    return end_time - start_time
+    execution_times[version] = end_time - start_time
+    print(f"{version.name} encrypted text: {encrypted}")
 
-plaintext = b"\x00\x04\x12\x14\x12\x04\x12\x00\x0C\x00\x13\x11\x08\x23\x19\x19"
-key_128 = b"\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87"
-key_320 = b"\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87\x13\xAA\x54\x87\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87\x13\xAA\x54\x87"
-key_384 = b"\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87\x13\xAA\x54\x87\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87\x13\xAA\x54\x87\x13\xAA\x54\x87\x13\xAA\x54\x87"
-key_512 = b"\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87\x24\x75\xA2\xB3\x34\x75\x56\x88\x31\xE2\x12\x00\x13\xAA\x54\x87"
+# Compare and print execution times
+for version, exec_time in execution_times.items():
+    print(f"Execution time for {version.name}: {exec_time:.9f} seconds")
 
-time_128_standard = measure_execution_time(AES.new(key_128, AES.MODE_ECB).encrypt, plaintext)
-time_128_custom = measure_execution_time(AES_base.encrypt, plaintext, key_128, AES_128.Nk, AES_128.Nr, AES_128.Rcon)
-time_320 = measure_execution_time(AES_base.encrypt, plaintext, key_320, AES_320.Nk, AES_320.Nr, AES_320.Rcon)
-time_384 = measure_execution_time(AES_base.encrypt, plaintext, key_384, AES_384.Nk, AES_384.Nr, AES_384.Rcon)
-time_512 = measure_execution_time(AES_base.encrypt, plaintext, key_512, AES_512.Nk, AES_512.Nr, AES_512.Rcon)
-
-print(f"Standard AES-128 Execution Time: {time_128_standard} seconds")
-print(f"Custom AES-128 Execution Time: {time_128_custom} seconds")
-print(f"AES-320 Execution Time: {time_320} seconds")
-print(f"AES-384 Execution Time: {time_384} seconds")
-print(f"AES-512 Execution Time: {time_512} seconds")
+# Determine the fastest AES version
+fastest_version = min(execution_times, key=execution_times.get)
+print(
+    f"The fastest AES version is {fastest_version.name}"
+    f" with an execution time of {execution_times[fastest_version]:.6f} seconds.")
